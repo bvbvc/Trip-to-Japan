711449a22444392ff99a7b3377e87a088171895d
bbbbb <bbb9870vvvv@gmail.com>
2018-09-15 13:40:55 +0000

    rails cでのユーザー登録機能完了

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+     before_save { self.e_mail.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :e_mail, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+   
+   
+   
+   has_secure_password
+   
+    mount_uploader :image, ImagesUploader
+   
+   
+end
