c59ead8b7c117837d9225726910064616f9a10ef
bbbbb <bbb9870vvvv@gmail.com>
2018-09-16 03:32:33 +0000

    プロフィール編集実装まえ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,15 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
+  
+  def edit
+    @user = User.find(current_user)
 
   private
 
